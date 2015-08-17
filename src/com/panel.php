fdd43a3c31f231d3c36f0b749348638b05bf59c8
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-17 21:55:51 +0430

    Update all panels

--- src/com/panel.php
+++ src/com/panel.php
@@ -3,5 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>University</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
     <link rel="stylesheet" type="text/css" href="../css/panel.css">
 </head>
