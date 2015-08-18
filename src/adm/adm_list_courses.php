7e9965444a82d8fd07c71b06ca7d1a50842b71a6
omid23 <omidbeigi23@gmail.com>
2015-08-18 13:38:13 -0700

    omid

--- src/adm/adm_list_courses.php
+++ src/adm/adm_list_courses.php
@@ -1,1 +1,42 @@
 
+<meta charset='utf-8'>
+<?php
+include('_conn.php');
+$sql = "
+    SELECT
+        teachers.name AS teacher,
+        lessons.name AS lesson,
+        courses.weekday,
+        courses.time
+    FROM
+        courses
+    JOIN
+        teachers
+    ON
+        courses.teacher = teachers.id
+    JOIN
+        lessons
+    ON
+        courses.lesson = lessons.id";
+$res = mysqli_query($db, $sql);
+?>
+
+
+<table>
+    <tr>
+        <th>نام استاد</th>
+        <th>نام درس</th>
+        <th>روز های هفته</th>
+        <th>نام درس</th>
+        <th>زمان</th>
+    </tr>
+    <?php foreach ($res as $row) { ?>
+        <tr>
+            <td><?php echo $row['teacher'] ?></td>
+            <td><?php echo $row['lesson'] ?></td>
+            <td><?php echo $row['weekday'] ?></td>
+            <td><?php echo $row['time'] ?></td>
+        </tr>    
+        <?php } ?>
+            
+</table>
