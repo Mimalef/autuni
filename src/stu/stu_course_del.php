e932f83f24a2acc00571b9aee126abe9d1f73e6f
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-19 13:29:06 +0430

    Add course delete

--- src/stu/stu_course_del.php
+++ src/stu/stu_course_del.php
@@ -1,1 +1,27 @@
+<?php
+include('stu_permission.php');
+include('../com/conn.php');
+include('stu_nav.php');
 
+$student = $_SESSION['student'];
+$course = $_GET['course'];
+
+$sql = "
+    DELETE FROM
+        takes
+    WHERE
+        student=$student
+    AND
+        course=$course";
+
+mysqli_query($db, $sql);
+
+if(mysqli_errno($db)==0)
+{
+    header("location: stu_take.php");
+}
+else
+{
+    echo "<p class='msg error'>عملیات نا موفق بود.</p>";
+}
+?>
