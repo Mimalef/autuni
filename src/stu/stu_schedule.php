ad65c66228e8be2d52f6a8dc12e319e5fb055ebb
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-19 12:26:53 +0430

    Add student schedule

--- src/stu/stu_schedule.php
+++ src/stu/stu_schedule.php
@@ -1,1 +1,51 @@
+<?php
+include('stu_permission.php');
+include('../com/conn.php');
+include('stu_nav.php');
 
+$student = $_SESSION['student'];
+
+$sql = "
+    SELECT
+        lessons.name AS lesson,
+        teachers.name AS teacher,
+        courses.weekday,
+        courses.time
+    FROM
+        takes
+    JOIN
+        courses
+    ON
+        takes.course = courses.id
+    JOIN
+        lessons
+    ON
+        courses.lesson = lessons.id
+    JOIN
+        teachers
+    ON
+        courses.teacher = teachers.id
+    WHERE
+        takes.student=$student";
+
+$res = mysqli_query($db, $sql);
+?>
+
+<div id="content">
+    <table>
+        <tr>
+            <th>درس</th>
+            <th>استاد</th>
+            <th>روز</th>
+            <th>ساعت</th>
+        </tr>
+        <?php foreach($res as $row) { ?>
+            <tr>
+                <td><?php echo $row['lesson'] ?></td>
+                <td><?php echo $row['teacher'] ?></td>
+                <td><?php echo $row['weekday'] ?></td>
+                <td><?php echo $row['time'] ?></td>
+            </tr>
+        <?php } ?>
+    </table>
+</div>
