a2ac85548f6726d3f504b477431589c5732736f0
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-19 13:29:35 +0430

    Add student payment

--- src/stu/stu_payment.php
+++ src/stu/stu_payment.php
@@ -1,1 +1,65 @@
+<?php
+include('stu_permission.php');
+include('../com/conn.php');
+include('stu_nav.php');
 
+$student = $_SESSION['student'];
+$sum = 0;
+
+$sql = "
+    SELECT
+        lessons.name AS lesson,
+        teachers.name AS teacher,
+        lessons.unit,
+        lessons.cost
+    FROM
+        takes
+    JOIN
+        courses
+    ON
+        takes.course = courses.id
+    JOIN
+        lessons
+    ON
+        courses.lesson = lessons.id
+    JOIN
+        teachers
+    ON
+        courses.teacher = teachers.id
+    WHERE
+        takes.student=$student";
+
+$res = mysqli_query($db, $sql);
+?>
+
+<div id="content">
+    <table>
+        <tr>
+            <th>درس</th>
+            <th>استاد</th>
+            <th>واحد</th>
+            <th>هزینه</th>
+        </tr>
+        <?php foreach ($res as $row) { ?>
+            <?php $sum += $row['cost'] ?>
+            <tr>
+                <td><?php echo $row['lesson'] ?></td>
+                <td><?php echo $row['teacher'] ?></td>
+                <td><?php echo $row['unit'] ?></td>
+                <td><?php echo $row['cost'] ?></td>
+            </tr>
+        <?php } ?>
+        <tr>
+            <td></td>
+            <td></td>
+            <td>جمع کل</td>
+            <td><?php echo $sum ?></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><input type="button" value="پرداخت"></td>
+        </tr>
+    </table>
+</div>
