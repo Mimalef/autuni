1b77e9faaff309f8743c6e0ef4531cdd72221341
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-19 12:25:53 +0430

    Fix students nav

--- src/stu/stu_nav.php
+++ src/stu/stu_nav.php
@@ -4,7 +4,7 @@
     <img id="logo" src="../img/logo.png">
     <a href="stu_schedule.php">برنامه هفتگی</a>
     <a href="stu_transcript.php">کارنامه</a>
-    <a href="stu_courses_list.php">لیست دروس</a>
+    <a href="stu_courses_list.php">لیست کلاس ها</a>
     <a href="stu_take.php">انتخاب واحد</a>
     <a href="stu_payment.php">پرداخت شهریه</a>
     <a href="../com/logout.php">خروج</a>
