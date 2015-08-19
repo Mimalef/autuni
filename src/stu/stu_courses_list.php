a02bd7e0d9a3cdeb19708dd0b8e981bdc40822a1
Mojtaba <mojtabaahmadi40@gmail.com>
2015-08-19 13:29:21 +0430

    Add courses list

--- src/stu/stu_courses_list.php
+++ src/stu/stu_courses_list.php
@@ -1,1 +1,48 @@
+<?php
+include('stu_permission.php');
+include('../com/conn.php');
+include('stu_nav.php');
 
+$student = $_SESSION['student'];
+
+$sql = "
+    SELECT
+        courses.id,
+        lessons.name AS lesson,
+        teachers.name AS teacher,
+        courses.weekday,
+        courses.time
+    FROM
+        courses
+    JOIN
+        lessons
+    ON
+        courses.lesson = lessons.id
+    JOIN
+        teachers
+    ON
+        courses.teacher = teachers.id";
+
+$res = mysqli_query($db, $sql);
+?>
+
+<div id="content">
+    <table>
+        <tr>
+            <th>کد کلاس</th>
+            <th>درس</th>
+            <th>استاد</th>
+            <th>روز</th>
+            <th>ساعت</th>
+        </tr>
+        <?php foreach($res as $row) { ?>
+            <tr>
+                <td><?php echo $row['id'] ?></td>
+                <td><?php echo $row['lesson'] ?></td>
+                <td><?php echo $row['teacher'] ?></td>
+                <td><?php echo $row['weekday'] ?></td>
+                <td><?php echo $row['time'] ?></td>
+            </tr>
+        <?php } ?>
+    </table>
+</div>
